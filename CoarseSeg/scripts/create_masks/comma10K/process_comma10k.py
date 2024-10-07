a899bb71951585cce0ceb79a70f254cded63fc2b
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-10-08 00:59:19 +0300

    adding logging, exceptions, error checking and input arguments

--- CoarseSeg/scripts/create_masks/comma10K/process_comma10k.py
+++ CoarseSeg/scripts/create_masks/comma10K/process_comma10k.py
@@ -1,6 +1,15 @@
 #! /usr/bin/env python3
 import pathlib
+import logging
+import sys
+sys.tracebacklimit = 0
+from argparse import ArgumentParser
 from PIL import Image
+
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = '%(levelname)s - %(message)s'
+)
 
 # Create coarse semantic segmentation mask
 # of combined classes
@@ -80,30 +89,96 @@ def createMask(colorMap, skyMap):
 
     return coarseSegColorMap
 
-# Reading dataset labels and images and sorting returned list in alphabetical order
-labels = sorted([f for f in pathlib.Path('/home/zain/Autoware/semantic_segmentation/training_data/comma10k/masks').glob("*.png")])
-images = sorted([f for f in pathlib.Path('/home/zain/Autoware/semantic_segmentation/training_data/comma10k/images/').glob("*.png")])
-sky_masks = sorted([f for f in pathlib.Path('/home/zain/Autoware/semantic_segmentation/training_data/comma10k/sky_masks').glob("*.png")])
+def main():
 
-# Paths to save training data with new coarse segmentation masks
-labels_save_path = '/home/zain/Autoware/semantic_segmentation/training_data/Coarse_Seg/comma10k/gt_masks/'
-images_save_path = '/home/zain/Autoware/semantic_segmentation/training_data/Coarse_Seg/comma10k/images/'
+    parser = ArgumentParser()
+    parser.add_argument("-l", "--labels", dest="labels_filepath", help="path to folder with input ground truth labels")
+    parser.add_argument("-sm", "--sky-masks", dest="sky_masks_filepath", help="path to folder with ground truth sky mask labels")
+    parser.add_argument("-i", "--images", dest="images_filepath", help="path to folder with input images")
+    parser.add_argument("-ls", "--labels-save", dest="labels_save_path", help="path to folder where processed labels will be saved")
+    parser.add_argument("-is", "--images-save", dest="images_save_path", help="path to folder where corresponding images will be saved")
+    args = parser.parse_args()
 
-# Looping through data
-for index in range(0, len(labels)):
+    # Paths to read input images and ground truth label masks from training data
+    labels_filepath = args.labels_filepath
+    images_filepath = args.images_filepath
+    sky_masks_filepath = args.sky_masks_filepath
 
-    # Open images and pre-existing masks
-    image = Image.open(str(images[index])).convert('RGB')
-    label = Image.open(str(labels[index]))
-    sky_mask = Image.open(str(sky_masks[index]))
+    # Paths to save training data with new coarse segmentation masks
+    labels_save_path = args.labels_save_path
+    images_save_path = args.images_save_path
 
-    # Create new Coarse Segmentation mask
-    coarseSegColorMap = createMask(label, sky_mask)
-  
-    # Save images
-    image.save(images_save_path + str(index) + ".png","PNG")
-    coarseSegColorMap.save(labels_save_path + str(index) + ".png","PNG")
+    # Reading dataset labels and images and sorting returned list in alphabetical order
+    labels = sorted([f for f in pathlib.Path(labels_filepath).glob("*.png")])
+    images = sorted([f for f in pathlib.Path(images_filepath).glob("*.png")])
+    sky_masks = sorted([f for f in pathlib.Path(sky_masks_filepath).glob("*.png")])
 
-    print(index)
+    # Checking validity
+    is_label_path_valid = False
+    is_image_path_valid = False
+    is_sky_mask_path_valid = False
+    is_data_valid = False
 
-print('--- COMPLETE ---')
+    # Getting number of labels and images
+    num_labels = len(labels)
+    num_images = len(images)
+    num_sky_masks = len(sky_masks)
+
+        # Checking if ground truth labels were read and logging error if missing
+    if (num_labels > 0):
+        logging.info(f'Found {num_labels} ground truth masks')
+        is_label_path_valid = True
+    else:
+        logging.error(f'No ground truth png masks found - check your labels filepath: {labels_filepath}')
+        raise ValueError('Input data is incorrect')
+
+    # Checking if input images were read and logging error if missing
+    if (num_images > 0):
+        logging.info(f'Found {num_images} input images')
+        is_image_path_valid = True
+    else:
+        logging.error(f'No input png images found - check your images filepath: {images_filepath}')
+        raise ValueError('Input data is incorrect')
+    
+    # Checking if sky masks were read and logging error if missing
+    if (num_sky_masks > 0):
+        logging.info(f'Found {num_sky_masks} ground truth sky masks')
+        is_sky_mask_path_valid = True
+    else:
+        logging.error(f'No input png sky masks found - check your sky masks filepath: {sky_masks_filepath}')
+        raise ValueError('Input data is incorrect')
+    
+    # Checking if number of ground truth labels, and sky masks the matches number of input images
+    # and logging error if mismatched
+    if (num_labels != num_images or num_sky_masks != num_images):
+        logging.error(f'Number of ground truth masks and sky masks: {num_labels} , {num_sky_masks} - does not match number of input images: {num_images}')
+        raise ValueError('Input data is incorrect')
+    else:
+        is_data_valid = True
+
+    # If all data checks have been passed
+    if(is_label_path_valid and is_image_path_valid and is_sky_mask_path_valid and is_data_valid):
+
+        logging.info('Beginning processing of data')
+
+        # Looping through data
+        for index in range(0, len(labels)):
+
+            # Open images and pre-existing masks
+            image = Image.open(str(images[index])).convert('RGB')
+            label = Image.open(str(labels[index]))
+            sky_mask = Image.open(str(sky_masks[index]))
+
+            # Create new Coarse Segmentation mask
+            coarseSegColorMap = createMask(label, sky_mask)
+        
+            # Save images
+            image.save(images_save_path + str(index) + ".png","PNG")
+            coarseSegColorMap.save(labels_save_path + str(index) + ".png","PNG")
+
+            logging.info(f'Processing image {index} of {num_images-1}')
+    
+        logging.info('----- Processing complete -----') 
+
+if __name__ == '__main__':
+    main()
