444391ba13da4888bbbd3f8cb6fbf6099833e675
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-10-09 22:12:42 +0300

    adding augmentations class to perform image augmentations

--- CoarseSeg/pytorch/train.py
+++ CoarseSeg/pytorch/train.py
@@ -4,10 +4,10 @@
 #! /usr/bin/env python3
 import torch
 from torchvision import transforms
-from PIL import Image
 import matplotlib.pyplot as plt
 from coarse_seg import CoarseSeg
 from data_utils.load_acdc import ACDC_Dataset
+from data_utils.augmentations import Augmentations
 
 # Checking devices (GPU vs CPU)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -22,13 +22,9 @@ def load_image(image):
     image = image.unsqueeze(0)
     return image.to(device)
 
-# Input Image Size
-row = 320
-col = 640
 
 loader = transforms.Compose(
     [
-        transforms.Resize((row,col)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]
@@ -38,9 +34,17 @@ loader = transforms.Compose(
 # Path to image
 acdc_labels_filepath= '/home/zain/Autoware/AutoSeg/training_data/Coarse_Seg/ACDC/gt_masks/'
 acdc_images_filepath = '/home/zain/Autoware/AutoSeg/training_data/Coarse_Seg/ACDC/images/'
+
+# Data loading helpers
 acdc_Dataset = ACDC_Dataset(acdc_labels_filepath, acdc_images_filepath)
 acdc_dataset_items = acdc_Dataset.getlen()
 image, label = acdc_Dataset.getitem(100)
+
+# Image augmentation
+augmentations = Augmentations(image, label)
+image, label = augmentations.getAugmentedData()
+
+# Loading to tensor format
 image_tensor = load_image(image)
 
 # Inference
@@ -58,7 +62,7 @@ size_all_mb = (param_size + buffer_size) / 1024**2
 print('model size: {:.3f}MB'.format(size_all_mb))
 print('params', param_size)
 
-# Visualise
+# Get Predictions
 prediction = prediction.squeeze(0).cpu().detach()
 prediction = prediction.permute(1, 2, 0)
 prediction_0 = prediction [:,:,0]
@@ -66,11 +70,13 @@ prediction_1 = prediction [:,:,1]
 prediction_2 = prediction [:,:,2]
 prediction_3 = prediction [:,:,3]
 
-
+# Visualise
 fig = plt.figure()
 plt.imshow(image)
+
 fig = plt.figure()
 plt.imshow(label)
+
 fig1, axs = plt.subplots(2,2)
 axs[0,0].imshow(prediction_0)
 axs[0,1].imshow(prediction_1)
