391a36799fdb5899c2c8507e0fd800146d87b8ee
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-10-28 07:24:29 +0300

    cleanup

--- Models/visualizations/video_visualization.py
+++ Models/visualizations/video_visualization.py
@@ -13,23 +13,22 @@ def make_visualization(prediction):
   
     row = shape[0]
     col = shape[1]
-    vis_predict = np.zeros((row, col, 3), dtype = "uint8")
-  
+    vis_predict_object = np.zeros((row, col, 3), dtype = "uint8")
+
     background_objects_colour = (255, 93, 61)
     foreground_objects_colour = (145, 28, 255)
-    road_colour = (220, 255, 0)
 
     # Extracting predicted classes and assigning to colourmap
     for x in range(row):
         for y in range(col):
             if(prediction[x,y].item() == 0):
-                vis_predict[x,y] = background_objects_colour
+                vis_predict_object[x,y] = background_objects_colour
             elif(prediction[x,y].item() == 1):
-                vis_predict[x,y] = foreground_objects_colour
+                vis_predict_object[x,y] = foreground_objects_colour
             elif(prediction[x,y].item() == 2):
-                vis_predict[x,y] = road_colour               
-    
-    return vis_predict
+                vis_predict_object[x,y] = background_objects_colour
+               
+    return vis_predict_object
 
 def main():  
 
@@ -41,8 +40,15 @@ def main():
     
   # Create a VideoCapture object and read from input file
   # If the input is taken from the camera, pass 0 instead of the video file name.
-  video_filepath = '/home/zain/Autoware/AutoSeg/videos/Driving_Scenes_Videos.mp4'
+  video_filepath = '/home/zain/Autoware/AutoSeg/videos/Driving_Scenes_Videos_2.mp4'
   cap = cv2.VideoCapture(video_filepath)
+
+  # Output filepath
+  output_filepath_obj = '/home/zain/Autoware/AutoSeg/videos/Driving_Scenes_Visualization_Objects_2.avi'
+
+  
+  writer_obj = cv2.VideoWriter(output_filepath_obj,
+    cv2.VideoWriter_fourcc(*"MJPG"), 25,(1280,720))
 
   # Check if video catpure opened successfully
   if (cap.isOpened()== False): 
@@ -63,22 +69,20 @@ def main():
       image_pil = image_pil.resize((640, 320))
       
       prediction = model.inference(image_pil)
-      vis = make_visualization(prediction)
-      vis = cv2.resize(vis, (1280, 720))
-      image_new = cv2.addWeighted(vis, alpha, frame, 1 - alpha, 0) 
-      cv2.imshow('Prediction', image_new)
+      vis_obj = make_visualization(prediction)
+      
+      vis_obj = cv2.resize(vis_obj, (1280, 720))
+      image_vis_obj = cv2.addWeighted(vis_obj, alpha, frame, 1 - alpha, 0)
+      cv2.imshow('Prediction Objects', image_vis_obj)
       cv2.waitKey(10)
-      # Press Q on keyboard to  exit
-      #if cv2.waitKey(25) & 0xFF == ord('q'):
-      #  break
+
+      # Writing to video frame
+      writer_obj.write(image_vis_obj)
   
-    # Break the loop
-    else: 
-      break
-  
-  # When everything done, release the video capture object
+  # When everything done, release the video capture and writer objects
   cap.release()
-  
+  writer_obj.release()
+
   # Closes all the frames
   cv2.destroyAllWindows()
 
