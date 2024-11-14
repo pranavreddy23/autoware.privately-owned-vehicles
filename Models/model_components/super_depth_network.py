b7e8640c34391910e9ea1e25b2765fdedd8e9e83
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-11-14 18:38:55 +0300

    adding overall SuperDepth network class

---
+++ Models/model_components/super_depth_network.py
@@ -0,0 +1,36 @@
+from .backbone import Backbone
+from .depth_supervision import DepthSupervision
+from .scene_context import SceneContext
+from .depth_neck import DepthNeck
+from .super_depth_head import SuperDepthHead
+import torch.nn as nn
+
+class SuperDepthNetwork(nn.Module):
+    def __init__(self):
+        super(SuperDepthNetwork, self).__init__()
+        
+        # Encoder
+        self.Backbone = Backbone()
+
+        # Depth Supervision
+        self.DepthSupervision = DepthSupervision()
+
+        # Context
+        self.SceneContext = SceneContext()
+
+        # Neck
+        self.DepthNeck = DepthNeck()
+
+        # Depth Head
+        self.SuperDepthHead = SuperDepthHead()
+    
+
+    def forward(self, image, pyramid_depth_features):
+        features = self.Backbone(image)
+        depth_supervision_features = self.DepthSupervision(pyramid_depth_features)
+        deep_features = features[4]
+        context = self.SceneContext(deep_features)
+        neck = self.DepthNeck(context, features, depth_supervision_features)
+        prediction, confidence, boundary = \
+            self.SuperDepthHead(neck, features, depth_supervision_features)
+        return prediction, confidence, boundary
