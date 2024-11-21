6c78723bb7368c9017822f44f3bc75806bf5cb0b
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-11-21 17:00:15 +0300

    adding data checks

--- SuperDepth/create_depth/MUAD/process_muad.py
+++ SuperDepth/create_depth/MUAD/process_muad.py
@@ -17,7 +17,30 @@ def main():
     num_depth_maps = len(depth_maps)
     num_images = len(images)
 
-    print('Found', num_depth_maps, 'depth maps and ', num_images, ' images')
+    is_depth_path_valid = False
+    is_image_path_valid = False
+    is_data_valid = False
+
+    # Checking if ground truth labels were read and logging error if missing
+    if (num_depth_maps > 0):
+        print(f'Found {num_depth_maps} ground truth masks')
+        is_depth_path_valid = True
+    else:
+        raise ValueError('No ground truth .exr depth maps found - check your depth filepath:')
+
+    # Checking if input images were read and logging error if missing
+    if (num_images > 0):
+        print(f'Found {num_images} input images')
+        is_image_path_valid = True
+    else:
+        raise ValueError('No input png images found - check your images filepath')
+
+    # Checking if number of ground truth labels matches number of input images
+    # and logging error if mismatched
+    if (num_depth_maps != num_images):
+        raise ValueError('Number of ground truth depth maps does not match number of input images:')
+    else:
+        is_data_valid = True
 
 if __name__ == '__main__':
     main()
