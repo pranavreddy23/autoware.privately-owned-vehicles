afea61eac19da33a6eaa09a9c867ef1e0227996e
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-11-23 20:49:17 +0300

    urban syn processing pipeline completed

--- SuperDepth/create_depth/UrbanSyn/process_urban_syn.py
+++ SuperDepth/create_depth/UrbanSyn/process_urban_syn.py
@@ -1,6 +1,9 @@
+#! /usr/bin/env python3
+#%%
 import pathlib
 import cv2
 from PIL import Image
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
@@ -63,6 +66,115 @@ def checkData(num_depth_maps, num_images):
     
     return check_passed
 
+def createDepthMap(depth_data):
+
+    # Getting size of depth data
+    size = depth_data.shape
+    height = size[0]
+    width = size[1]
+
+    # Converting depth data to metric depth values
+    depth_map = Image.fromarray(depth_data)
+    depth_map = np.asarray(depth_map, dtype=np.float32)
+    depth_map = 100000 * depth_map
+
+    # Removing erroneous depth data
+    max_depth = np.max(depth_map)
+    for i in range(0, height):
+        for j in range(0, width):
+            if (depth_map[i,j] <= 0):
+                depth_map[i,j] = max_depth
+
+    return depth_map
+
+def findDepthBoundaries(depth_map):
+
+    # Getting size of depth map
+    size = depth_map.shape
+    height = size[0]
+    width = size[1]
+
+    # Initializing depth boundary mask
+    depth_boundaries = np.zeros_like(depth_map, dtype=np.uint8)
+
+    # Fiding depth boundaries
+    for i in range(1, height-1):
+        for j in range(1, width-1):
+
+            # Finding derivative
+            x_grad = depth_map[i-1,j] - depth_map[i+1, j]
+            y_grad = depth_map[i,j-1] - depth_map[i, j+1]
+            grad = abs(x_grad) + abs(y_grad)
+            
+            # Derivative threshold
+            if(grad > 10):
+                depth_boundaries[i,j] = 255
+
+    return depth_boundaries
+
+def createHeightMap(depth_map, max_height, min_height):
+
+    # Getting size of depth map
+    size = depth_map.shape
+    height = size[0]
+    width = size[1]
+
+    # Projection centre for Y-axis
+    cy = round(height/2)
+
+    # Initializing height-map
+    height_map = np.zeros_like(depth_map)
+
+    # Height of camera above ground plane
+    camera_height = 2 
+    
+    for i in range(0, height):
+        for j in range(0, width):
+            depth_val = depth_map[i, j]
+            H = (cy-i)*(depth_val)/1024
+            height_map[i,j] = H + camera_height
+    
+    # Clipping height values for dataset
+    height_map = height_map.clip(min = min_height, max = max_height)
+    
+    return height_map
+
+def createSparseSupervision(image, height_map, max_height, min_height, depth_map):
+
+    # Getting size of height map
+    size = height_map.shape
+    height = size[0]
+    width = size[1]
+
+    # Initializing depth boundary mask
+    sparse_supervision = np.zeros_like(height_map)
+
+    # Getting pixel access for image
+    px = image.load()
+
+    # Fiding image gradients
+    for i in range(1, width-1):
+        for j in range(1, height-1):
+
+            # Finding image derivative - using green colour channel
+            x_grad = px[i-1,j][1] - px[i+1, j][1]
+            y_grad = px[i,j-1][1] - px[i, j+1][1]
+            grad = abs(x_grad) + abs(y_grad)
+
+            # Derivative threshold to find likely stereo candidates
+            if(grad > 25):
+                sparse_supervision[j,i] = height_map[j,i]
+            else:
+                sparse_supervision[j,i] = max_height
+
+            # Max height threshold
+            if(height_map[j,i] == max_height):
+                sparse_supervision[j,i] = max_height
+    
+    # Clipping height values for dataset
+    sparse_supervision = sparse_supervision.clip(min = min_height, max = max_height)
+    return sparse_supervision
+
 def main():
 
     # Filepaths for data loading and savind
@@ -89,7 +201,48 @@ def main():
     if(check_passed):
 
         print('Beginning processing of data')
+
+        # Looping through data
+        for index in range(0, num_images):
+
+            print(f'Processing image {index} of {num_images-1}')
+            
+            # Open images and pre-existing masks
+            image = Image.open(str(images[index]))
+            depth_data = cv2.imread(str(depth_maps[index]), cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+
+            # Create metric depth map and height map
+            depth_map = createDepthMap(depth_data)
+            depth_boundaries = findDepthBoundaries(depth_map)
+            max_height = 7
+            min_height = -0.5
+            height_map = createHeightMap(depth_map, max_height, min_height)
+            sparse_supervision = createSparseSupervision(image, height_map, max_height, min_height, depth_map)
+
+            # Save files
+            # RGB Image as PNG
+            image_save_path = root_save_path + '/image/' + str(index) + '.png'
+            image.save(image_save_path, "PNG")
+
+            # Depth map as binary file in .npy format
+            depth_save_path = root_save_path + '/depth/' + str(index) + '.npy'
+            np.save(depth_save_path, depth_map)
+
+            # Height map as binary file in .npy format
+            height_save_path = root_save_path + '/height/' + str(index) + '.npy'
+            np.save(height_save_path, height_map)
+
+            # Sparse supervision map as binary file in .npy format
+            supervision_save_path = root_save_path + '/supervision/' + str(index) + '.npy'
+            np.save(supervision_save_path, sparse_supervision)
+
+            # Boundary mask as PNG
+            boundary_save_path = root_save_path + '/boundary/' + str(index) + '.png'
+            boundary_mask = Image.fromarray(depth_boundaries)
+            boundary_mask.save(boundary_save_path, "PNG")
         
+        print('----- Processing complete -----') 
+      
 
 
 if __name__ == '__main__':
