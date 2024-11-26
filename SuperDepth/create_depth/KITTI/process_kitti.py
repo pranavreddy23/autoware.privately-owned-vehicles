006935867e5369040189da7440e9542f3947a5f0
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-11-26 16:45:39 +0300

    filtering sparse lidar depth projection to create ground truth depth map

--- SuperDepth/create_depth/KITTI/process_kitti.py
+++ SuperDepth/create_depth/KITTI/process_kitti.py
@@ -3,14 +3,11 @@
 import pathlib
 import numpy as np
 from PIL import Image
-
-def createDepthMap(depth_data):
-
-    assert(np.max(depth_data) > 255)
-
-    depth_map = depth_data.astype(np.float) / 256.
-    depth_map[depth_data == 0] = -1.
-    return depth_map
+import matplotlib.pyplot as plt
+import cv2
+import sys
+sys.path.append('../../../')
+from Models.data_utils.check_data import CheckData
 
 def removeExtraSamples(image_folders):
     
@@ -23,6 +20,58 @@ def removeExtraSamples(image_folders):
             filtered_images.append(images[j])
 
     return filtered_images
+
+def createDepthMap(depth_data):
+
+    assert(np.max(depth_data) > 255)
+
+    depth_map = depth_data.astype('float32') / 256.
+
+    depth_map = filterDepthMap(depth_map)
+    return depth_map
+
+def filterDepthMap(depth_map):
+
+    erosion_kernel = np.ones((3,3), np.uint8) 
+    depth_map = cv2.dilate(depth_map, erosion_kernel, iterations=4) 
+
+    closing_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19,19))
+    depth_map = cv2.morphologyEx(depth_map, cv2.MORPH_CLOSE, closing_kernel)
+    
+    # Getting size of depth map
+    size = depth_map.shape
+    height = size[0]
+    width = size[1]
+
+    # Interpolate along height
+    for j in range(0, width):
+        interp_depth = 0
+        for i in range(height-1, 1, -1):
+
+            lower_depth = depth_map[i,j]
+            upper_depth = depth_map[i-1, j]
+
+            if(lower_depth != 0 and upper_depth == 0):
+                interp_depth = lower_depth
+            
+            if(interp_depth != 0 and upper_depth == 0):
+                depth_map[i-1, j] = interp_depth
+    
+    depth_map = cv2.medianBlur(depth_map, 5)
+    return depth_map
+
+def cropData(image, depth_map):
+
+    # Getting size of depth map
+    size = depth_map.shape
+    height = size[0]
+    width = size[1]
+
+    # Cropping out those parts of data for which depth is unavailable
+    depth_map = depth_map[:, 100 : width - 100]
+    image = image.crop((100, 0, width - 100, height-1))
+
+    return image, depth_map
 
 def main():
 
@@ -44,7 +93,29 @@ def main():
     # If all data checks have been passed
     num_depth_maps = len(depth_maps)
     num_images = len(images)
-    print(num_images, num_depth_maps)
+
+    check_passed = CheckData(num_images, num_depth_maps)
+
+    if(check_passed):
+
+        print('Beginning processing of data')
+        # Looping through data
+        for index in range(0, 1):
+
+            print(f'Processing image {index} of {num_images-1}')
+            
+            # Open images and pre-existing masks
+            image = Image.open(str(images[index]))
+            depth_data = np.array(Image.open(str(depth_maps[index])), dtype=int)
+            depth_map = createDepthMap(depth_data)
+            image, depth_map = cropData(image, depth_map)
+
+            plt.figure()
+            plt.imshow(image)
+            plt.title('Image')
+            plt.figure()
+            plt.imshow(depth_map, cmap='inferno')
+            plt.title('Depth')
     
 
 if __name__ == '__main__':
