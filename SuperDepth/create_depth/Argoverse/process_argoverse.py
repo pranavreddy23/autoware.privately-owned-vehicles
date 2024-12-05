239561138a09970c9fa6705f57d45badd0f86257
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-12-05 16:03:13 +0300

    reading ground truth disparity maps, left stereo images and right stereo images, checking data

--- SuperDepth/create_depth/Argoverse/process_argoverse.py
+++ SuperDepth/create_depth/Argoverse/process_argoverse.py
@@ -15,6 +15,30 @@ def main():
     # Filepaths for data loading and savind
     root_data_path = '/mnt/media/Argoverse/'
 
+    # Paths to read ground truth depth and input images from training data
+    depth_filepath = root_data_path + 'disparity_maps_v1.1/'
+    images_filepath = root_data_path + 'rectified_stereo_images_v1.1/train/'
+
+    # Reading dataset labels and images and sorting returned list in alphabetical order
+    depth_maps = sorted([f for f in pathlib.Path(depth_filepath).glob("*/stereo_front_left_rect_disparity/*.png")])
+    images_left = sorted([f for f in pathlib.Path(images_filepath).glob("*/stereo_front_left_rect/*.jpg")])
+    images_right = sorted([f for f in pathlib.Path(images_filepath).glob("*/stereo_front_right_rect/*.jpg")])
+
+    # If all data checks have been passed
+    num_depth_maps = len(depth_maps)
+    num_images_left = len(images_left)
+    num_images_right = len(images_right)
+
+    check_data_left = CheckData(num_images_left, num_depth_maps)
+    check_data_right = CheckData(num_images_right, num_depth_maps)
+
+    check_passed_left = check_data_left.getCheck()
+    check_passed_right = check_data_right.getCheck()
+
+    if(check_passed_left and check_passed_right):
+
+        print('Beginning processing of data')
+
 if __name__ == '__main__':
     main()
 #%%
