43e3f47a8f81004db4488ba0c1d3116b5ba4861b
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-12-06 16:47:30 +0300

    reading images and ground truth depth data

--- SuperDepth/create_depth/MUSES/process_muses.py
+++ SuperDepth/create_depth/MUSES/process_muses.py
@@ -3,6 +3,7 @@
 import pathlib
 import numpy as np
 from PIL import Image
+import json
 import sys
 sys.path.append('../../../')
 from Models.data_utils.check_data import CheckData
@@ -14,6 +15,25 @@ def main():
     
     # Filepaths for data loading and saving
     root_data_path = '/mnt/media/MUSES/'
+
+    # Paths to read ground truth depth and input images from training data
+    depth_filepath = root_data_path + 'lidar/'
+    images_filepath = root_data_path + 'frame_camera/'
+    calib_path =  root_data_path + 'calib.json'
+
+    # Reading dataset labels and images and sorting returned list in alphabetical order
+    depth_maps = sorted([f for f in pathlib.Path(depth_filepath).glob("*.bin")])
+    images = sorted([f for f in pathlib.Path(images_filepath).glob("*.png")])
+
+    # If all data checks have been passed
+    num_depth_maps = len(depth_maps)
+    num_images = len(images)
+   
+    checkData = CheckData(num_images, num_depth_maps)
+    check_passed = checkData.getCheck()
+
+    if(check_passed):
+        print('Beginning processing of data')
 
 if __name__ == '__main__':
     main()
