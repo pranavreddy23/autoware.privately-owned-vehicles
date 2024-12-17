2f64ef0ec5178756f093a57b78cc2354526fd0b0
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-12-17 21:41:51 +0300

    removing unused function

--- SuperDepth/create_depth/MUSES/process_muses.py
+++ SuperDepth/create_depth/MUSES/process_muses.py
@@ -103,42 +103,6 @@ def create_image_from_point_cloud(uv_img_cords_filtered, filtered_pcd_points, ta
 
     return image
 
-def createSparseSupervision(image, height_map, max_height, min_height):
-
-    # Getting size of height map
-    size = height_map.shape
-    height = size[0]
-    width = size[1]
-
-    # Initializing depth boundary mask
-    sparse_supervision = np.zeros_like(height_map)
-
-    # Getting pixel access for image
-    px = image.load()
-
-    # Fiding image gradients
-    for i in range(1, width-1):
-        for j in range(1, height-1):
-
-            # Finding image derivative - using green colour channel
-            x_grad = px[i-1,j][1] - px[i+1, j][1]
-            y_grad = px[i,j-1][1] - px[i, j+1][1]
-            grad = abs(x_grad) + abs(y_grad)
-
-            # Derivative threshold to find likely stereo candidates
-            if(grad > 25):
-                sparse_supervision[j,i] = height_map[j,i]
-            else:
-                sparse_supervision[j,i] = max_height
-
-            # Max height threshold
-            if(height_map[j,i] == max_height):
-                sparse_supervision[j,i] = max_height
-    
-    # Clipping height values for dataset
-    sparse_supervision = sparse_supervision.clip(min = min_height, max = max_height)
-    return sparse_supervision
-
 def cropData(image_left, depth_map, depth_boundaries, height_map, sparse_supervision):
 
     # Cropping out those parts of data for which depth is unavailable
