1d3394739839e1e94264dc5d65049570c6b8a4bf
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2024-12-29 17:24:34 +0300

    using pre-trained SceneSeg weights in SuperDepth

--- Models/training/train_super_depth.py
+++ Models/training/train_super_depth.py
@@ -3,50 +3,59 @@
 #! /usr/bin/env python3
 import torch
 import matplotlib.pyplot as plt
-import numpy as np
+from torchvision import transforms
+from pytorch_model_summary import summary
+from PIL import Image
 import sys
 sys.path.append('..')
 from model_components.super_depth_network import SuperDepthNetwork
+from model_components.scene_seg_network import SceneSegNetwork
 
 
 def main():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Using {device} for inference')
-        
-    model = SuperDepthNetwork()
+
+    # Load pre-trained weights
+    sceneSegNetwork = SceneSegNetwork()
+    root_path = '/home/zain/Autoware/Privately_Owned_Vehicles/Models/exports/SceneSeg/run_1_batch_decay_Oct18_02-46-35/'
+    pretrained_checkpoint_path = root_path + 'iter_140215_epoch_4_step_15999.pth'
+    sceneSegNetwork.load_state_dict(torch.load \
+        (pretrained_checkpoint_path, weights_only=True, map_location=device))
+    
+    # Instantiate Model with pre-trained weights
+    model = SuperDepthNetwork(sceneSegNetwork)
+    print(summary(model, torch.zeros((1, 3, 320, 640)), show_input=True))
     model = model.to(device)
 
-    test_data_image = torch.rand((1, 3, 320, 640)).to(device)
+    # Random input
+    input_image_filepath = '/mnt/media/SuperDepth/UrbanSyn/image/10.png'
+    image = Image.open(input_image_filepath)
+    image = image.resize((640, 320))
 
-    test_data_pyramid_features = []
-    feature_0 = torch.rand((1, 1, 320, 640)).to(device)
-    feature_1 = torch.rand((1, 1, 160, 320)).to(device)
-    feature_2 = torch.rand((1, 1, 80, 160)).to(device)
-    feature_3 = torch.rand((1, 1, 40, 80)).to(device)
-    feature_4 = torch.rand((1, 1, 20, 40)).to(device)
-    feature_5 = torch.rand((1, 1, 10, 20)).to(device)
+    # Image loader
+    image_loader = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  
+        ]
+    )
 
-    test_data_pyramid_features.append(feature_0)
-    test_data_pyramid_features.append(feature_1)
-    test_data_pyramid_features.append(feature_2)
-    test_data_pyramid_features.append(feature_3)
-    test_data_pyramid_features.append(feature_4)
-    test_data_pyramid_features.append(feature_5)
+    image_tensor = image_loader(image)
+    image_tensor = image_tensor.unsqueeze(0)
+    image_tensor = image_tensor.to(device)
 
-    prediction, boundary = model(test_data_image, test_data_pyramid_features)
+    prediction = model(image_tensor)
 
     prediction = prediction.squeeze(0).cpu().detach()
     prediction = prediction.permute(1, 2, 0)
-    prediction = prediction.numpy()
-    prediction = prediction + abs(np.min(prediction))
 
-    boundary = boundary.squeeze(0).cpu().detach()
-    boundary = boundary.permute(1, 2, 0)
+    plt.figure()
+    plt.imshow(image)
+    plt.figure()
+    plt.imshow(prediction)
 
-    fig, axarr = plt.subplots(1, 2)
-    axarr[0].imshow(prediction)
-    axarr[1].imshow(boundary)
     
 if __name__ == '__main__':
     main()
