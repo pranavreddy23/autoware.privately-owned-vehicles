c3244e71848aa8b2e9b96c3961bd196bfe572b29
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-01-09 11:33:26 +0300

    adjusting morphology parameters

--- SuperDepth/create_depth/common/lidar_depth_fill.py
+++ SuperDepth/create_depth/common/lidar_depth_fill.py
@@ -10,10 +10,10 @@ class LidarDepthFill():
 
         # Filter kernels
         erosion_kernel = np.ones((3,3), np.uint8) 
-        closing_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19,19))
+        closing_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15,15))
 
         # Morphology filters
-        self.depth_map = cv2.dilate(self.depth_map, erosion_kernel, iterations=4) 
+        self.depth_map = cv2.dilate(self.depth_map, erosion_kernel, iterations=3) 
         self.depth_map = cv2.morphologyEx(self.depth_map, cv2.MORPH_CLOSE, closing_kernel)
         
         # Getting size of depth map
