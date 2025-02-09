583a374cc721dfd17642ccb9041b05de8ba19cd7
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-02-09 13:42:45 +0300

    adjusitng definition of overall model based on new DepthContext block and PreTrainedBackbone

--- Models/model_components/scene_3d_network.py
+++ Models/model_components/scene_3d_network.py
@@ -1,6 +1,8 @@
+from .pre_trained_backbone import PreTrainedBackbone
+from .depth_context import DepthContext
 from .scene_3d_neck import Scene3DNeck
 from .scene_3d_head import Scene3DHead
-from .scene_3d_upstream import Scene3DUpstream
+
 import torch.nn as nn
 
 class Scene3DNetwork(nn.Module):
@@ -8,7 +10,10 @@ class Scene3DNetwork(nn.Module):
         super(Scene3DNetwork, self).__init__()
 
         # Upstream blocks
-        self.SuperDepthUpstream = Scene3DUpstream(pretrained)
+        self.PreTrainedBackbone = PreTrainedBackbone(pretrained)
+
+        # Depth Context
+        self.DepthContext = DepthContext()
 
         # Neck
         self.DepthNeck = Scene3DNeck()
@@ -18,7 +23,9 @@ class Scene3DNetwork(nn.Module):
     
 
     def forward(self, image):
-        features, context = self.SuperDepthUpstream(image)
+        features = self.PreTrainedBackbone(image)
+        deep_features = features[4]
+        context = self.DepthContext(deep_features)
         neck = self.DepthNeck(context, features)
         prediction = self.SuperDepthHead(neck, features)
         return prediction
