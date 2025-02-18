26c6268cb7baf0f7ca8ffcdd5d7551029ae7d938
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-02-18 13:45:34 +0300

    adding Argoverse dataset as test set and including scale factor for benchmarking

--- Models/training/test_validate_scene_3d.py
+++ Models/training/test_validate_scene_3d.py
@@ -18,19 +18,19 @@ def main():
     kitti_labels_filepath = root + 'KITTI/height/'
     kitti_images_filepath = root + 'KITTI/image/'
     kitti_validities_filepath = root + 'KITTI/validity/'
+    s_kitti = 1.556
 
     # DDAD
     ddad_labels_filepath = root + 'DDAD/height/'
     ddad_images_filepath = root + 'DDAD/image/'
     ddad_validities_filepath = root + 'DDAD/validity/'
-
-    # URBANSYN
-    urbansyn_labels_fileapath = root + 'UrbanSyn/height/'
-    urbansyn_images_fileapath = root + 'UrbanSyn/image/'
+    s_ddad_f = 0.805
+    s_ddad_b = 1.452
 
     # ARGOVERSE
     argoverse_labels_filepath = root + '/Argoverse/height/'
     argoverse_images_filepath = root + '/Argoverse/image/'
+    s_argoverse = 0.556
 
     # KITTI - Data Loading
     kitti_Dataset = LoadDataScene3D(kitti_labels_filepath, kitti_images_filepath, 
@@ -41,18 +41,14 @@ def main():
     ddad_Dataset = LoadDataScene3D(ddad_labels_filepath, ddad_images_filepath, 
                                            'DDAD', ddad_validities_filepath)
     _, ddad_num_val_samples = ddad_Dataset.getItemCount()
-
-    # URBANSYN - Data Loading
-    urbansyn_Dataset = LoadDataScene3D(urbansyn_labels_fileapath, urbansyn_images_fileapath, 'URBANSYN')
-    _, urbansyn_num_val_samples = urbansyn_Dataset.getItemCount()
+    _, ddad_val_cams = ddad_Dataset.getDDADCameras()
 
     # ARGOVERSE - Data Loading
     argoverse_Dataset = LoadDataScene3D(argoverse_labels_filepath, argoverse_images_filepath, 'ARGOVERSE')
     argoverse_num_train_samples, argoverse_num_val_samples = argoverse_Dataset.getItemCount()
 
     # Total validation samples
-    total_val_samples = kitti_num_val_samples + \
-        ddad_num_val_samples + urbansyn_num_val_samples
+    total_val_samples = kitti_num_val_samples + ddad_num_val_samples 
     print(total_val_samples, ': total validation samples')
 
     # Total testing Samples
@@ -62,8 +58,8 @@ def main():
     
     # Pre-trained model checkpoint path
     root_path = \
-        '/home/zain/Autoware/Privately_Owned_Vehicles/Models/exports/Scene3D/2025_01_27/model/'
-    checkpoint_path = root_path + 'iter_1091507_epoch_26_step_39599.pth'
+        '/home/zain/Autoware/Privately_Owned_Vehicles/Models/exports/Scene3D/2025_02_13/model/'
+    checkpoint_path = root_path + 'iter_1719999_epoch_37_step_27361.pth'
 
     
     # Trainer Class
@@ -80,7 +76,6 @@ def main():
     running_mAE_overall = 0
     running_mAE_kitti = 0
     running_mAE_ddad = 0
-    running_mAE_urbansyn = 0
     running_mAE_argoverse = 0
     
     # No gradient calculation
@@ -91,7 +86,7 @@ def main():
             image_val, gt_val, validity_val = kitti_Dataset.getItemVal(val_count)
 
             # Run Validation and calculate mAE Score
-            mAE = trainer.validate(image_val, gt_val, validity_val)
+            mAE = trainer.validate(image_val, gt_val, validity_val, s_kitti)
 
             # Accumulating mAE score
             running_mAE_kitti += mAE
@@ -102,21 +97,17 @@ def main():
             image_val, gt_val, validity_val = ddad_Dataset.getItemVal(val_count)
 
             # Run Validation and calculate mAE Score
-            mAE = trainer.validate(image_val, gt_val, validity_val)
+            s_ddad = 1.0
+            if(ddad_val_cams[val_count] == 'back_camera'):
+                s_ddad = s_ddad_b
+            elif(ddad_val_cams[val_count] == 'front_camera'):
+                s_ddad = s_ddad_f
+
+            # Run Validation and calculate mAE Score
+            mAE = trainer.validate(image_val, gt_val, validity_val, s_ddad)
 
             # Accumulating mAE score
             running_mAE_ddad += mAE
-            running_mAE_overall += mAE
-
-        # URBANSYN
-        for val_count in range(0, urbansyn_num_val_samples):
-            image_val, gt_val, validity_val = urbansyn_Dataset.getItemVal(val_count)
-            
-            # Run Validation and calculate mAE Score
-            mAE = trainer.validate(image_val, gt_val, validity_val)
-
-            # Accumulating mAE score
-            running_mAE_urbansyn += mAE
             running_mAE_overall += mAE
 
         # Validate
@@ -124,19 +115,19 @@ def main():
 
         # ARGOVERSE
         for test_count in range(0, argoverse_num_val_samples):
-            image_test, gt_test, validity_test = urbansyn_Dataset.getItemVal(test_count)
+            image_test, gt_test, validity_test = argoverse_Dataset.getItemVal(test_count)
             
             # Run Validation and calculate mAE Score
-            mAE = trainer.validate(image_test, gt_test, validity_test)
+            mAE = trainer.validate(image_test, gt_test, validity_test, s_argoverse)
 
             # Accumulating mAE score
             running_mAE_argoverse += mAE
 
         for test_count in range(0, argoverse_num_train_samples):
-            image_test, gt_test, validity_test = urbansyn_Dataset.getItemTrain(test_count)
+            image_test, gt_test, validity_test = argoverse_Dataset.getItemTrain(test_count)
             
             # Run Validation and calculate mAE Score
-            mAE = trainer.validate(image_test, gt_test, validity_test)
+            mAE = trainer.validate(image_test, gt_test, validity_test, s_argoverse)
 
             # Accumulating mAE score
             running_mAE_argoverse += mAE
@@ -148,14 +139,12 @@ def main():
         avg_mAE_overall = running_mAE_overall/total_val_samples
         avg_mAE_kitti = running_mAE_kitti/kitti_num_val_samples
         avg_mAE_ddad = running_mAE_ddad/ddad_num_val_samples
-        avg_mAE_urbansyn = running_mAE_urbansyn/urbansyn_num_val_samples
         avg_mAE_argoverse = running_mAE_argoverse/total_test_samples
 
         print('--- Validation Scores ---')
         print('Overall: ', avg_mAE_overall)
         print('KITTI: ', avg_mAE_kitti)
         print('DDAD:', avg_mAE_ddad)
-        print('URBANSYN: ', avg_mAE_urbansyn)
 
         print('--- Testing Scores ---')
         print('ARGOVERSE: ', avg_mAE_argoverse)         
