05b2153a9590f0e0d7f000a4ef2a60c641a8331a
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-03-07 21:51:55 +0300

    adding full EgoPath network

---
+++ Models/model_components/ego_path_network.py
@@ -0,0 +1,26 @@
+from .pre_trained_backbone import PreTrainedBackbone
+from .path_context import PathContext
+from .ego_path_head import EgoPathHead
+
+import torch.nn as nn
+
+class EgoPathNetwork(nn.Module):
+    def __init__(self, pretrained):
+        super(EgoPathNetwork, self).__init__()
+
+        # Upstream blocks
+        self.PreTrainedBackbone = PreTrainedBackbone(pretrained)
+
+        # Path Context
+        self.PathContext = PathContext()
+
+        # EgoPath Head
+        self.EgoPathHead = EgoPathHead()
+    
+
+    def forward(self, image):
+        features = self.PreTrainedBackbone(image)
+        deep_features = features[4]
+        context = self.PathContext(deep_features)
+        prediction = self.EgoPathHead(context)
+        return prediction
