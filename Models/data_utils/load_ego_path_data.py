88e61fe6cffa0015792bda1508803b68d9799255
TranHuuNhatHuy <huy9515@gmail.com>
2025-03-11 21:37:04 +0900

    implement sanity check

--- Models/data_utils/load_ego_path_data.py
+++ Models/data_utils/load_ego_path_data.py
@@ -10,6 +10,7 @@ from PIL import Image, ImageDraw
 import warnings
 from datetime import datetime
 from typing import Literal, get_args
+from .check_data import CheckData
 
 VALID_DATASET_LITERALS = Literal[
     "BDD100K", 
@@ -42,28 +43,25 @@ class LoadDataEgoPath():
         # ================= Preliminary checks ================= #
 
         if not (0 <= self.val_set_fraction <= 1):
-            raise ValueError(f"val_set_fraction must be within [0, 1]. Current set to {self.val_set_fraction}")
+            raise ValueError(f"val_set_fraction must be within [0, 1]. Currently set to {self.val_set_fraction}")
 
         if not (self.dataset_name in VALID_DATASET_LIST):
             raise ValueError("Unknown dataset! Contact our team so we can work on this.")
         
         with open(self.label_filepath, "r") as f:
-            self.label_list = json.load(f)
-        self.image_list = sorted([
+            self.labels = json.load(f)
+        self.images = sorted([
             f for f in pathlib.Path(self.image_dirpath).glob("*.png")
         ])
 
-        self.N_labels = len(self.label_list)
-        self.N_images = len(self.image_list)
+        self.N_labels = len(self.labels)
+        self.N_images = len(self.images)
 
-        if (self.N_labels != self.N_images):
-            raise ValueError(f"Number of images ({self.N_images}) does not match number of labels ({self.N_labels})")
-        
-        if (self.N_labels == 0):
-            raise ValueError(f"No labels found in specified path {self.label_filepath}")
-        
-        if (self.N_images == 0):
-            raise ValueError(f"No images found in specified path {self.image_dirpath}")
+        # Sanity check func by Mr. Zain
+        checkData = CheckData(
+            self.N_images,
+            self.N_labels
+        )
         
         # ================= Initiate data loading ================= #
 
