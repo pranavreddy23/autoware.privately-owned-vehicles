00bf9372671529544725aedffe9ea80ee84ec2af
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-04-24 19:49:54 +0300

    removing separate noise transform for keypoints

--- Models/data_utils/augmentations.py
+++ Models/data_utils/augmentations.py
@@ -72,23 +72,6 @@ class Augmentations():
             ]
         )
 
-        self.transform_noise_keypoints = A.Compose(
-            [      
-                A.MultiplicativeNoise(multiplier=(0.5, 1.5), per_channel=False, p=0.25),
-                A.PixelDropout(dropout_prob=0.025, per_channel=True, p=0.25),
-                A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.6, hue=0.2, p=0.25),
-                A.GaussNoise(var_limit=(50.0, 100.0), mean=0, noise_scale_factor=0.2, p=0.25),
-                A.GaussNoise(var_limit=(250.0, 250.0), mean=0, noise_scale_factor=1, p=0.25),
-                A.ISONoise(color_shift=(0.1, 0.5), intensity=(0.5, 0.5), p=0.25),
-                A.RandomFog(fog_coef_lower=0.1, fog_coef_upper=0.3, alpha_coef=0.2, p=0.25),
-                A.RandomFog(fog_coef_lower=0.7, fog_coef_upper=1.0, alpha_coef=0.04, p=0.25),
-                A.RandomRain(p=0.1),
-                A.Spatter(mean=(0.65, 0.65), std=(0.3, 0.3), gauss_sigma=(2, 2), \
-                    cutout_threshold=(0.68, 0.68), intensity=(0.3, 0.3), mode='rain', \
-                    p=0.1),
-                A.ToGray(num_output_channels=3, method='weighted_average', p=0.1)           
-            ]
-        )
 
     # SEMANTIC SEGMENTATION
     # Set data values
