db762878fc69bfc2659ba9977a9fac327d689262
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-05-12 11:24:37 +0300

    adjusting augmentations class initializer to use new type BINARY_SEGMENTATION

--- Models/training/domain_seg_trainer.py
+++ Models/training/domain_seg_trainer.py
@@ -105,14 +105,13 @@ class DomainSegTrainer():
 
         if(is_train):
             # Augmenting Data for training
-            augTrain = Augmentations(is_train=True, data_type='SEGMENTATION')
+            augTrain = Augmentations(is_train=True, data_type='BINARY_SEGMENTATION')
             augTrain.setDataSeg(self.image, self.gt)
             self.image, self.augmented  = \
                 augTrain.applyTransformSeg(image=self.image, ground_truth=self.gt)
-
         else:
             # Augmenting Data for testing/validation
-            augVal = Augmentations(is_train=False, data_type='SEGMENTATION')
+            augVal = Augmentations(is_train=False, data_type='BINARY_SEGMENTATION')
             augVal.setDataSeg(self.image, self.gt)
             self.image, self.augmented = \
                 augVal.applyTransformSeg(image=self.image, ground_truth=self.gt)
