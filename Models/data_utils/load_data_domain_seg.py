bff78c3f1339b8c882a3130523706da94efd4345
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-05-27 15:02:07 +0300

    getting train and val samples

--- Models/data_utils/load_data_domain_seg.py
+++ Models/data_utils/load_data_domain_seg.py
@@ -42,3 +42,27 @@ class LoadDataSceneSeg():
                     self.train_images.append(str(self.images[count]))
                     self.train_labels.append(str(self.labels[count]))
                     self.num_train_samples += 1
+
+    # Getting number of train/val samples
+    def getItemCount(self):
+        return self.num_train_samples, self.num_val_samples
+    
+    # Get training data in numpy format
+    def getItemTrain(self, index):
+        train_image = Image.open(str(self.train_images[index])).convert('RGB')
+
+        train_mask = Image.open(str(self.train_labels[index]))
+        fn = lambda x : 255 if x > 0 else 0
+        train_ground_truth = train_mask.convert('L').point(fn, mode='1')
+
+        return  np.array(train_image), np.array(train_ground_truth)
+    
+    # Get training data in numpy format
+    def getItemVal(self, index):
+        val_image = Image.open(str(self.val_images[index])).convert('RGB')
+
+        val_mask = Image.open(str(self.val_labels[index]))
+        fn = lambda x : 255 if x > 0 else 0
+        val_ground_truth = val_mask.convert('L').point(fn, mode='1')
+
+        return  np.array(val_image), np.array(val_ground_truth)
