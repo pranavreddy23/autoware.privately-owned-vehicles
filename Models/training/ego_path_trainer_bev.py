3e0144367bf2bbf0f2cf889d64dccb54d0de2b7b
TranHuuNhatHuy <huy9515@gmail.com>
2025-06-16 07:57:14 +0900

    add trainer aux imports

---
+++ Models/training/ego_path_trainer_bev.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import torch
+from torchvision import transforms
+from torch import nn, optim
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import math
+import cv2
+from PIL import Image
+import numpy as np
+import sys
+
+sys.path.append('..')
+from model_components.scene_seg_network import SceneSegNetwork
+from model_components.ego_path_network import EgoPathNetwork
+from data_utils.augmentations import Augmentations
