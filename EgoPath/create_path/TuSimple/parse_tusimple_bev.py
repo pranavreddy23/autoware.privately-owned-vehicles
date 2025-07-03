a49c8e6dd8aed73f42b921d4d424325154410412
TranHuuNhatHuy <huy9515@gmail.com>
2025-07-03 23:27:38 +0900

    add func roundLineFloats

--- EgoPath/create_path/TuSimple/parse_tusimple_bev.py
+++ EgoPath/create_path/TuSimple/parse_tusimple_bev.py
@@ -14,3 +14,26 @@ def custom_warning_format(message, category, filename, lineno, line=None):
     return f"WARNING : {message}\n"
 
 warnings.formatwarning = custom_warning_format
+
+PointCoords = tuple[float, float]
+ImagePointCoords = tuple[int, int]
+
+# Skipped frames
+skipped_dict = {}
+
+# ============================== Helper functions ============================== #
+
+
+def log_skipped(frame_id, reason):
+    skipped_dict[frame_id] = reason
+
+def roundLineFloats(line, ndigits = 4):
+    line = list(line)
+    for i in range(len(line)):
+        line[i] = [
+            round(line[i][0], ndigits),
+            round(line[i][1], ndigits)
+        ]
+    line = tuple(line)
+    return line
+
