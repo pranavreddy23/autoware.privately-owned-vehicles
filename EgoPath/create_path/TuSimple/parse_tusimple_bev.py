76e49fa21c2dae6e2c5b0c8affaa4a7d5e03ffda
TranHuuNhatHuy <huy9515@gmail.com>
2025-07-03 23:25:16 +0900

    add custom warning

--- EgoPath/create_path/TuSimple/parse_tusimple_bev.py
+++ EgoPath/create_path/TuSimple/parse_tusimple_bev.py
@@ -9,3 +9,8 @@ import warnings
 import numpy as np
 from PIL import Image, ImageDraw
 
+# Custom warning format cuz the default one is wayyyyyy too verbose
+def custom_warning_format(message, category, filename, lineno, line=None):
+    return f"WARNING : {message}\n"
+
+warnings.formatwarning = custom_warning_format
