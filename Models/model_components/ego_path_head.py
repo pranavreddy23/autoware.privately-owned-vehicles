c76009560145557d196616fd95ad0caa576f4e37
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-07-04 15:35:12 +0300

    adjusting EgoPath head to account for new bev_path_context

--- Models/model_components/ego_path_head.py
+++ Models/model_components/ego_path_head.py
@@ -10,12 +10,9 @@ class EgoPathHead(nn.Module):
         self.dropout = nn.Dropout(p=0.25)
 
         # Context - MLP Layers
-        self.ego_path_layer_0 = nn.Linear(1280, 800)
-        self.ego_path_layer_1 = nn.Linear(800, 800)
-        self.ego_path_layer_2 = nn.Linear(800, 200)
-        self.ego_path_layer_3 = nn.Linear(200, 33)
-        self.ego_path_layer_4 = nn.Linear(200, 33)
-        self.ego_path_layer_5 = nn.Linear(200, 33)
+        self.ego_path_layer_0 = nn.Linear(800, 200)
+        self.ego_path_layer_1 = nn.Linear(200, 33)
+ 
 
     def forward(self, features):
         # Pooling and averaging channel layers to get a single vector
@@ -23,18 +20,8 @@ class EgoPathHead(nn.Module):
 
         # MLP
         p0 = self.ego_path_layer_0(feature_vector)
-        p0 = self.dropout(p0)
         p0 = self.GeLU(p0)
-        p1 = self.ego_path_layer_1(p0)
-        p1 = self.dropout(p1)
-        p1 = self.GeLU(p1)
-        p2 = self.ego_path_layer_2(p1)
-        feature = self.GeLU(p2)
-
-        # Output
-        ego_path = self.ego_path_layer_3(feature)
-        left_lane = self.ego_path_layer_4(feature)
-        right_lane = self.ego_path_layer_5(feature)
+        ego_path = self.ego_path_layer_1(p0)
 
         # Final result
-        return ego_path, left_lane, right_lane
+        return ego_path
