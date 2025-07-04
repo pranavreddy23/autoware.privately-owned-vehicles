b711eb3d50245b89b026d65f74c259b974a0a71c
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-07-04 15:40:53 +0300

    creating EgoSteer overall network which combines the outputs from EgoLanes and EgoPath head modules together, giving a joint prediction of lanes and path

---
+++ Models/model_components/ego_steer_network.py
@@ -0,0 +1,31 @@
+from .pre_trained_backbone import PreTrainedBackbone
+from .bev_path_context import BEVPathContext
+from .ego_lanes_head import EgoLanesHead
+from .ego_path_head import EgoPathHead
+
+import torch.nn as nn
+
+class EgoSteerNetwork(nn.Module):
+    def __init__(self, pretrained):
+        super(EgoSteerNetwork, self).__init__()
+
+        # Upstream blocks
+        self.PreTrainedBackbone = PreTrainedBackbone(pretrained)
+
+        # Path Context
+        self.PathContext = BEVPathContext()
+
+        # EgoLanes Head
+        self.EgoLanesHead = EgoLanesHead()
+
+        # EgoPath Head
+        self.EgoPathHead = EgoPathHead()
+    
+
+    def forward(self, image):
+        features = self.PreTrainedBackbone(image)
+        deep_features = features[4]
+        context = self.PathContext(deep_features)
+        ego_path = self.EgoPathHead(context)
+        ego_left_lane, ego_right_lane = self.EgoLanesHead(context)
+        return ego_path, ego_left_lane, ego_right_lane
