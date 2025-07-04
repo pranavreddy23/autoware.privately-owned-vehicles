08db14333fbe98cb9237f375783653f482e3785e
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-07-04 15:35:36 +0300

    updating context to bev_path_context and adjusting output returned by ego_path_head

--- Models/model_components/ego_path_network.py
+++ Models/model_components/ego_path_network.py
@@ -1,5 +1,5 @@
 from .pre_trained_backbone import PreTrainedBackbone
-from .path_context import PathContext
+from .bev_path_context import BEVPathContext
 from .ego_path_head import EgoPathHead
 
 import torch.nn as nn
@@ -12,7 +12,7 @@ class EgoPathNetwork(nn.Module):
         self.PreTrainedBackbone = PreTrainedBackbone(pretrained)
 
         # Path Context
-        self.PathContext = PathContext()
+        self.PathContext = BEVPathContext()
 
         # EgoPath Head
         self.EgoPathHead = EgoPathHead()
@@ -22,5 +22,5 @@ class EgoPathNetwork(nn.Module):
         features = self.PreTrainedBackbone(image)
         deep_features = features[4]
         context = self.PathContext(deep_features)
-        ego_path, left_lane, right_lane = self.EgoPathHead(context)
-        return ego_path, left_lane, right_lane
+        ego_path = self.EgoPathHead(context)
+        return ego_path
