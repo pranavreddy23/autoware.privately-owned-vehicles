7b58a4eaef5658cdbcc34ea4a9d8e86b5451a090
JITERN <limjitern@gmail.com>
2025-07-27 23:21:56 +0800

    initial commit
    Signed-off-by: JITERN <limjitern@gmail.com>

---
+++ VisionPilot/ROS2/PATHFINDER/include/PATHFINDER/estimator.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <array>
+#include <cmath>
+
+constexpr size_t STATE_DIM = 13;
+
+struct Gaussian
+{
+    double mean;
+    double variance;
+};
+
+class Estimator
+{
+private:
+    std::array<Gaussian, STATE_DIM> state;
+    std::vector<std::pair<size_t, size_t>> fusion_rules;
+
+public:
+    void initialize(const std::array<Gaussian, STATE_DIM> &init_state);
+    void predict(const std::array<Gaussian, STATE_DIM> &process);
+    void update(const std::array<Gaussian, STATE_DIM> &measurement);
+    void configureFusionGroups(const std::vector<std::pair<size_t, size_t>> &rules);
+    const std::array<Gaussian, STATE_DIM> &getState() const;
+};
---
+++ VisionPilot/ROS2/PATHFINDER/src/pathfinder_node.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "path_finder.hpp"
+#include "estimator.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world PATHFINDER package\n");
+  return 0;
+}
