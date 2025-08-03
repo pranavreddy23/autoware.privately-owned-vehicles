e0eee35a5101a3b7261f68188a033154195610f9
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-08-03 23:14:12 +0300

    reverting to best train model layers

--- Models/model_components/bev_path_context.py
+++ Models/model_components/bev_path_context.py
@@ -8,26 +8,26 @@ class BEVPathContext(nn.Module):
         # Standard
         self.GeLU = nn.GELU()
         self.sigmoid = nn.Sigmoid()
-        self.dropout = nn.Dropout(p=0.8)
+        self.dropout = nn.Dropout(p=0.25)
         self.avg_pool = nn.AvgPool2d(2, stride=2)
 
         # Feature convolution
         self.feature_conv = nn.Conv2d(1280, 1280, 3, 1, 1)
 
         # Context - MLP Layers
-        #self.context_layer_0 = nn.Linear(1280, 800)
-        #self.context_layer_1 = nn.Linear(800, 800)
-        #self.context_layer_2 = nn.Linear(800, 200)
+        self.context_layer_0 = nn.Linear(1280, 800)
+        self.context_layer_1 = nn.Linear(800, 800)
+        self.context_layer_2 = nn.Linear(800, 200)
 
         # Context - Extraction Layers
-        #self.context_layer_3 = nn.Conv2d(1, 128, 3, 1, 1)
-        #self.context_layer_4 = nn.Conv2d(128, 256, 3, 1, 1)
-        #self.context_layer_5 = nn.Conv2d(256, 512, 3, 1, 1)
-        #self.context_layer_6 = nn.Conv2d(512, 1280, 3, 1, 1)
+        self.context_layer_3 = nn.Conv2d(1, 128, 3, 1, 1)
+        self.context_layer_4 = nn.Conv2d(128, 256, 3, 1, 1)
+        self.context_layer_5 = nn.Conv2d(256, 512, 3, 1, 1)
+        self.context_layer_6 = nn.Conv2d(512, 1280, 3, 1, 1)
 
         # Context - Decode layers
-        self.context_layer_7 = nn.Linear(1280, 1280)
-        self.context_layer_8 = nn.Linear(1280, 1280)
+        self.context_layer_7 = nn.Linear(1280, 800)
+        self.context_layer_8 = nn.Linear(800, 800)
      
 
     def forward(self, features):
@@ -38,7 +38,7 @@ class BEVPathContext(nn.Module):
         # Pooling and averaging channel layers to get a single vector
         feature_vector = torch.mean(bev_features, dim = [2,3])
         feature_vector = self.dropout(feature_vector)
-        '''
+       
         # MLP
         c0 = self.context_layer_0(feature_vector)
         c0 = self.dropout(c0)
@@ -79,14 +79,5 @@ class BEVPathContext(nn.Module):
         path_features = self.context_layer_8(path_features)
         path_features = self.dropout(path_features)
         path_features = self.GeLU(path_features)
-        '''
 
-        # Decoding driving path related features
-        path_features = self.context_layer_7(feature_vector)
-        path_features = self.dropout(path_features)
-        path_features = self.GeLU(path_features)
-     
-        path_features = self.context_layer_8(path_features)
-        path_features = self.dropout(path_features)
-        path_features = self.GeLU(path_features)
         return path_features   
