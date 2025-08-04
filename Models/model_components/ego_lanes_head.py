81eb63da74066562c6a3b758c0d405c47f94e272
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-08-04 10:54:35 +0300

    removing dropout from head layers

--- Models/model_components/ego_lanes_head.py
+++ Models/model_components/ego_lanes_head.py
@@ -7,7 +7,6 @@ class EgoLanesHead(nn.Module):
         super(EgoLanesHead, self).__init__()
         # Standard
         self.GeLU = nn.GELU()
-        self.dropout = nn.Dropout(p=0.5)
 
         # Context - MLP Layers
         self.ego_left_lane_layer_0 = nn.Linear(800, 200)
@@ -24,18 +23,14 @@ class EgoLanesHead(nn.Module):
         # MLP
         p0_left = self.ego_left_lane_layer_0(feature_vector)
         p0_left = self.GeLU(p0_left)
-        p0_left = self.dropout(p0_left)
         p1_left = self.ego_left_lane_layer_1(p0_left)
         p1_left = self.GeLU(p1_left)
-        p1_left = self.dropout(p1_left)
         ego_left_lane = self.ego_left_lane_layer_2(p1_left)
 
         p0_right = self.ego_right_lane_layer_0(feature_vector)
         p0_right = self.GeLU(p0_right)
-        p0_right = self.dropout(p0_right)
         p1_right = self.ego_right_lane_layer_1(p0_right)
         p1_right = self.GeLU(p1_right)
-        p1_right = self.dropout(p1_right)
         ego_right_lane = self.ego_right_lane_layer_2(p1_right)
 
 
