3670ee8c24356ce0b03c00f12935ee6664fade78
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-08-04 15:29:59 +0300

    simplifying head for EgoPath and EgoLanes and adding 0.25 dropout for BEV Context

--- Models/model_components/ego_path_head.py
+++ Models/model_components/ego_path_head.py
@@ -6,23 +6,16 @@ class EgoPathHead(nn.Module):
     def __init__(self):
         super(EgoPathHead, self).__init__()
         # Standard
-        self.GeLU = nn.GELU()
         self.Tanh = nn.Tanh()
 
         # Context - MLP Layers
-        self.ego_path_layer_0 = nn.Linear(800, 200)
-        self.ego_path_layer_1 = nn.Linear(200, 200)
-        self.ego_path_layer_2 = nn.Linear(200, 11)
+        self.ego_path_layer_0 = nn.Linear(800, 11)
  
 
     def forward(self, feature_vector):
 
         # MLP
-        p0 = self.ego_path_layer_0(feature_vector)
-        p0 = self.GeLU(p0)
-        p1 = self.ego_path_layer_1(p0)
-        p1 = self.GeLU(p1)
-        p1 = self.ego_path_layer_2(p1)
-        ego_path = self.Tanh(p1)*3
+        ego_path = self.ego_path_layer_0(feature_vector)
+        ego_path = self.Tanh(ego_path)*3
 
         return ego_path
